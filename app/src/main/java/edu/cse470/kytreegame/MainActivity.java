b8ff509c8022af6071ba4a8d69dc35815b6ce24e
andygutterman <andygutterman@outlook.com>
2024-05-01 15:54:42 -0400

    Added streak and highscore text views, sharedprefs to save these, removed a redundant image, formatting fixes.

--- app/src/main/java/edu/cse470/kytreegame/MainActivity.java
+++ app/src/main/java/edu/cse470/kytreegame/MainActivity.java
@@ -1,19 +1,16 @@
 package edu.cse470.kytreegame;
 
+import android.content.SharedPreferences;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.graphics.Color;
 
-
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
@@ -37,21 +34,29 @@ public class MainActivity extends AppCompatActivity {
     private String currentTreeFolder;
     private List<String> currentTreeNames;
     private String correctTreeName;
-
+    private boolean isGameWon = false;
     private int currentStreak = 0;
+    private TextView highScoreTextView;
+    private TextView highScoreLabelTextView;
+    private TextView streakLabelTextView;
+    private TextView streakTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         Window window = getWindow();
         window.setStatusBarColor(ContextCompat.getColor(this, R.color.deep_blue));
 
+        highScoreLabelTextView = findViewById(R.id.highScoreLabelTextView);
+        highScoreTextView = findViewById(R.id.highScoreTextView);
 
-        // Initialize the views
+        streakLabelTextView = findViewById(R.id.streakLabelTextView);
+        streakTextView = findViewById(R.id.streakTextView);
+        currentStreak = loadStreak();
+
         treeImageView1 = findViewById(R.id.treeImageView1);
         treeImageView2 = findViewById(R.id.treeImageView2);
         button1 = findViewById(R.id.button1);
@@ -59,31 +64,35 @@ public class MainActivity extends AppCompatActivity {
         button3 = findViewById(R.id.button3);
         feedbackTextView = findViewById(R.id.feedbackTextView);
 
-        // Initialize the random object
         random = new Random();
 
-        // Initialize the tree folders from assets
         try {
             AssetManager assetManager = getAssets();
             treeFolders = Arrays.asList(assetManager.list("trees"));
-            startGame();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        restoreGameState();
     }
 
-    // Function to start a new game
+
+    private void restoreGameState() {
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        currentTreeFolder = sharedPreferences.getString("currentTreeFolder", "");
+        correctTreeName = sharedPreferences.getString("correctTreeName", "");
+        currentStreak = sharedPreferences.getInt("currentStreak", 0);
+        isGameWon = sharedPreferences.getBoolean("isGameWon", false);
+        startGame();
+    }
+
     private void startGame() {
-        // Reset the game won state
         isGameWon = false;
 
-        // Clear the feedback text
         feedbackTextView.setText("");
-
-        // Reset the background colors and text colors of the buttons to their default state
+        streakTextView.setText(String.valueOf(currentStreak));
         resetButtonColors();
 
-        // Select a random folder from tree folders to be the correct tree
         int randomIndex = random.nextInt(treeFolders.size());
         currentTreeFolder = treeFolders.get(randomIndex);
         correctTreeName = currentTreeFolder;
@@ -97,7 +106,6 @@ public class MainActivity extends AppCompatActivity {
         String incorrectTreeName1 = allTreeNames.get(0);
         String incorrectTreeName2 = allTreeNames.get(1);
 
-        // Assign correct and incorrect tree names to buttons
         List<String> buttonOptions = Arrays.asList(correctTreeName, incorrectTreeName1, incorrectTreeName2);
         Collections.shuffle(buttonOptions);
 
@@ -105,7 +113,6 @@ public class MainActivity extends AppCompatActivity {
         button2.setText(buttonOptions.get(1));
         button3.setText(buttonOptions.get(2));
 
-        // List the image files in the chosen folder
         String[] imageFiles;
         try {
             AssetManager assetManager = getAssets();
@@ -115,85 +122,36 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
+        updateHighScoreText();
+        currentStreak = loadStreak();
+
+
         // Select three distinct image files from the current folder
         List<String> imageFileList = Arrays.asList(imageFiles);
         Collections.shuffle(imageFileList);
         List<String> chosenImages = imageFileList.subList(0, 3);
 
-        // Display the images for the current folder
         displayRandomTreeImages(chosenImages);
-    }// Function to reset button colors to default
-
-    public void onTreeButtonClick(View view) {
-        // Get the clicked button
-        Button clickedButton = (Button) view;
-        // Get the selected tree name from the button text
-        String selectedTreeName = clickedButton.getText().toString();
-
-        if (isGameWon) {
-            // If the game has been won and the correct button is clicked again, start a new game
-            startGame();
-            // Reset the isGameWon flag
-            isGameWon = false;
-            return; // Exit the function
-        }
-
-        // Check if the selected tree name matches the correct tree name
-        if (selectedTreeName.equals(correctTreeName)) {
-            // Correct answer
-            // Increment the streak counter
-            currentStreak++;
-
-            // Set the feedback text to include the current streak
-            feedbackTextView.setText("Correct! (Streak: " + currentStreak + ")\nThis is a " + correctTreeName + ".");
-            feedbackTextView.setTextColor(getResources().getColor(R.color.custom_green));
-
-            // Change the button background color to custom green and text color to white
-            clickedButton.setBackgroundColor(getResources().getColor(R.color.custom_green));
-            clickedButton.setTextColor(getResources().getColor(R.color.custom_text_white));
-
-            // Set isGameWon to true to indicate that the game has been won
-            isGameWon = true;
-        } else {
-            // Incorrect answer
-            // Reset the streak counter
-            currentStreak = 0;
-
-            // Set the feedback text
-            feedbackTextView.setText("Incorrect\nThis is not a " + selectedTreeName + " tree.");
-            feedbackTextView.setTextColor(getResources().getColor(R.color.custom_red));
-
-            // Change the button background color to custom red and text color to white
-            clickedButton.setBackgroundColor(getResources().getColor(R.color.custom_red));
-            clickedButton.setTextColor(getResources().getColor(R.color.custom_text_white));
-        }
     }
 
-    private void resetButtonColors() {
-        // Get the deep blue color from the color resource
-        int defaultButtonColor = getResources().getColor(R.color.deep_blue);
-        int defaultTextColor = getResources().getColor(android.R.color.white); // White text color
-
-        // Set the background and text colors of the buttons
-        button1.setBackgroundColor(defaultButtonColor);
-        button1.setTextColor(defaultTextColor);
-        button2.setBackgroundColor(defaultButtonColor);
-        button2.setTextColor(defaultTextColor);
-        button3.setBackgroundColor(defaultButtonColor);
-        button3.setTextColor(defaultTextColor);
-    }
-
-
-
-
-    // Function to display random tree images
     private void displayRandomTreeImages(List<String> chosenImages) {
         try {
             AssetManager assetManager = getAssets();
+            List<String> imageFileList = new ArrayList<>(chosenImages); // Create a copy of chosenImages
 
-            // Open the image files as InputStreams using the chosen image file names
-            InputStream inputStream1 = assetManager.open("trees/" + currentTreeFolder + "/" + chosenImages.get(0));
-            InputStream inputStream2 = assetManager.open("trees/" + currentTreeFolder + "/" + chosenImages.get(1));
+            // Shuffle the list of images
+            Collections.shuffle(imageFileList);
+
+            // Ensure the indices of the two chosen images are different
+            int index1 = random.nextInt(imageFileList.size());
+            int index2;
+            do {
+                index2 = random.nextInt(imageFileList.size());
+            } while (index2 == index1);
+
+            // Open the input streams for the chosen images
+            InputStream inputStream1 = assetManager.open("trees/" + currentTreeFolder + "/" + imageFileList.get(index1));
+            InputStream inputStream2 = assetManager.open("trees/" + currentTreeFolder + "/" + imageFileList.get(index2));
 
             // Decode the images and set them to the ImageViews
             Bitmap bitmap1 = BitmapFactory.decodeStream(inputStream1);
@@ -201,7 +159,6 @@ public class MainActivity extends AppCompatActivity {
             treeImageView1.setImageBitmap(bitmap1);
             treeImageView2.setImageBitmap(bitmap2);
 
-            // Close the InputStreams
             inputStream1.close();
             inputStream2.close();
         } catch (IOException e) {
@@ -209,13 +166,108 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private int getHighScore() {
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        return sharedPreferences.getInt("highScore", 0); // 0 is the default value if high score is not found
+    }
 
-    // Function to handle button clicks
+    private int loadStreak() {
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        return sharedPreferences.getInt("currentStreak", 0);
+    }
 
-    private boolean isGameWon = false; // Boolean variable to track if the game is in a winning state
+    private void saveHighScore(int highScore) {
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("highScore", highScore);
+        editor.apply();
+    }
+
+    private void saveStreak(int streak) {
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("currentStreak", streak);
+        editor.apply();
+    }
 
 
+    private void updateHighScoreText() {
+        int highScore = getHighScore();
+        highScoreTextView.setText(String.valueOf(highScore));
+        highScoreLabelTextView.setText(" HIGH\nSCORE");
+        streakLabelTextView.setText("    THIS\n STREAK");
+    }
 
+    public void onTreeButtonClick(View view) {
+        Button clickedButton = (Button) view;
+        String selectedTreeName = clickedButton.getText().toString();
 
+        if (isGameWon) {
+            startGame();
+            isGameWon = false;
+            return;
+        }
+
+        if (selectedTreeName.equals(correctTreeName)) {
+            currentStreak++;
+            streakTextView.setText(String.valueOf(currentStreak));
+            saveStreak(currentStreak);
+
+            if (currentStreak > getHighScore()) {
+                saveHighScore(currentStreak);
+                updateHighScoreText();
+            }
+            correctFeedback(selectedTreeName, clickedButton);
+            isGameWon = true;
+
+        } else {
+            // Reset streak to 0 when the user selects the wrong tree
+            currentStreak = 0;
+            streakTextView.setText(String.valueOf(currentStreak));
+            saveStreak(currentStreak);
+            wrongFeedback(selectedTreeName, clickedButton);
+        }
+    }
+
+    private Button wrongFeedback(String selectedTreeName, Button clickedButton){
+        String article = startsWithVowel(selectedTreeName) ? "an" : "a";
+        feedbackTextView.setText("This is not " + article + " " + selectedTreeName + " tree.");
+        feedbackTextView.setTextColor(getResources().getColor(R.color.custom_red));
+
+        clickedButton.setBackgroundColor(getResources().getColor(R.color.custom_red));
+        clickedButton.setTextColor(getResources().getColor(R.color.custom_text_white));
+        return clickedButton;
+    }
+
+    private Button correctFeedback(String incorrectTreeName, Button clickedButton){
+        String article = startsWithVowel(incorrectTreeName) ? "an" : "a";
+        feedbackTextView.setText("This is " + article + " " + incorrectTreeName + " tree.");
+        feedbackTextView.setTextColor(getResources().getColor(R.color.custom_green));
+
+        clickedButton.setBackgroundColor(getResources().getColor(R.color.custom_green));
+        clickedButton.setTextColor(getResources().getColor(R.color.custom_text_white));
+        return clickedButton;
+    }
+
+    private boolean startsWithVowel(String word) {
+        // Convert the word to lowercase for case insensitivity
+        word = word.toLowerCase();
+        // Check if the first character is a vowel
+        char firstChar = word.charAt(0);
+        return firstChar == 'a' || firstChar == 'e' || firstChar == 'i' || firstChar == 'o' || firstChar == 'u';
+    }
+
+    private void resetButtonColors() {
+        // Get the deep blue color from the color resource
+        int defaultButtonColor = getResources().getColor(R.color.deep_blue);
+        int defaultTextColor = getResources().getColor(android.R.color.white); // White text color
+
+        button1.setBackgroundColor(defaultButtonColor);
+        button1.setTextColor(defaultTextColor);
+        button2.setBackgroundColor(defaultButtonColor);
+        button2.setTextColor(defaultTextColor);
+        button3.setBackgroundColor(defaultButtonColor);
+        button3.setTextColor(defaultTextColor);
+    }
 
 }
