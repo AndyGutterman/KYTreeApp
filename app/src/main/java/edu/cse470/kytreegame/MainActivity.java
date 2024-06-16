6f06f6f763c3c458507605d0cb315b2799d28edb
andygutterman <andygutterman@outlook.com>
2024-06-15 23:48:34 -0400

    Changed main menu buttons to appear more consistent across device resolutions

--- app/src/main/java/edu/cse470/kytreegame/MainActivity.java
+++ app/src/main/java/edu/cse470/kytreegame/MainActivity.java
@@ -1,11 +1,13 @@
 package edu.cse470.kytreegame;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.Button;
-import android.graphics.drawable.ColorDrawable;
-
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -19,6 +21,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        Configuration configuration = getResources().getConfiguration();
+        int screenSize = configuration.screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK;
+        if (screenSize == Configuration.SCREENLAYOUT_SIZE_SMALL || screenSize == Configuration.SCREENLAYOUT_SIZE_NORMAL) {
+
+            TextView appNameTextView = findViewById(R.id.native_trees_textview);
+            Button galleryButton = findViewById(R.id.tree_gallery_button);
+            Button startButton = findViewById(R.id.start_game_button);
+
+            appNameTextView.setTextSize(18);
+            startButton.setTextSize(16);
+            galleryButton.setTextSize(16);
+        }
 
         Window window = getWindow();
         window.setStatusBarColor(ContextCompat.getColor(this, R.color.goldenrod_gold));
@@ -35,8 +50,8 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, GalleryActivity.class);
         startActivity(intent);
     }
+
     private void startGame() {
-        // Start the game activity or perform other actions
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
     }
